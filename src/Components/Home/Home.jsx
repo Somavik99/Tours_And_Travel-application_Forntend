421e64ca5c3b113cda99994447ac7f356faae15b
Avik Roy <somavik99@gmail.com>
2024-07-27 20:35:57 +0530

    main

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,59 @@
 import NavBar from "../Navbar/NavBar";
+import worldPng from "../../assets/world.png";
+import HeroImage1 from "../../assets/hero-img01.jpg";
+import HeroVideo from "../../assets/hero-video.mp4";
+import HeroImage2 from "../../assets/hero-img02.jpg";
+import "./Home.css";
 
 function Home() {
   return (
     <>
       <NavBar />
-      <h1>HOME</h1>
+      <div className="Hero__container">
+        <div>
+          <p>
+            <span
+              style={{
+                background: "orange",
+                padding: "5px 15px",
+                borderRadius: "15px",
+                fontSize:"18px",
+                fontFamily:"sans-serif"
+              }}
+            >
+              Know Before You Go
+            </span>
+            <img src={worldPng} alt="world" style={{ width: "40px" }} />
+          </p>
+          <h1 style={{ width: "600px", fontSize: "55px" }}>
+            Traveling open the doors to creating
+            <span style={{ color: "orange" }}> memories</span>
+          </h1>
+          <p
+            style={{
+              width: "500px",
+              fontSize: "20px",
+              fontWeight: "400",
+              color: "gray",
+            }}
+          >
+            Tours and travels offer an enriching way to explore the world,
+            providing unique experiences and unforgettable memories. From exotic
+            beaches to historical landmarks, each destination has its own charm
+            and story. Travel agencies and tour operators simplify the journey,
+            arranging accommodations, transportation, and guided tours. Group
+            tours provide social opportunities, while solo travel allows for
+            personal exploration.
+          </p>
+        </div>
+        <div className="HeroImage__container">
+          <img src={HeroImage1} alt="heroImage" className="Hero__image1" />
+          <video controls className="video">
+            <source src={HeroVideo} type="video/mp4" />
+          </video>
+          <img src={HeroImage2} alt="HeroImage" className="Hero__image2" />
+        </div>
+      </div>
     </>
   );
 }
