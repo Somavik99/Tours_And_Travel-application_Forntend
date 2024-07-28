17aae5247f32618e52786563b52116989cf8c819
Avik Roy <somavik99@gmail.com>
2024-07-28 14:13:26 +0530

    main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,17 +1,68 @@
-import Search from "../Search/Search"
-
+import Search from "../Search/Search";
+import weatherIcon from "../../assets/weather.png";
+import tourGuideIcon from "../../assets/guide.png";
+import customizationIcon from "../../assets/customization.png";
+import "./Main.css";
 
 function Main() {
   return (
     <>
-    <div>
-      <Search/>
-    </div>
-    <div>
-        
-    </div>
+      <div>
+        <Search />
+      </div>
+      <div className="Main__body">
+        <div>
+          <p
+            style={{
+              fontSize: "15px",
+              fontStyle: "italic",
+              color: "red",
+              fontFamily: "Playwrite BE WAL ",
+            }}
+          >
+            What we serve
+          </p>
+          <h1>We offer our best services</h1>
+        </div>
+        <div className="Main__bodyCard">
+          <div>
+            <p className="img__container">
+              <img src={weatherIcon} alt="weather" />
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+              illo ad facere assumenda, dignissimos amet quae expedita nisi.
+              Molestias, doloribus.
+            </p>
+          </div>
+        </div>
+        <div className="Main__bodyCard">
+          <div>
+            <p className="img__container">
+              <img src={tourGuideIcon} alt="tourGuide" />
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+              illo ad facere assumenda, dignissimos amet quae expedita nisi.
+              Molestias, doloribus.
+            </p>
+          </div>
+        </div>
+        <div className="Main__bodyCard">
+          <div>
+            <p className="img__container">
+              <img src={customizationIcon} alt="customization" />
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+              illo ad facere assumenda, dignissimos amet quae expedita nisi.
+              Molestias, doloribus.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
